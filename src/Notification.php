7138d55ef93931e702e7005d8ef8e7fe8bd89c4c
zhangzhenqian <jasonzhang081114@gmail.com>
2017-12-08 14:33:03 +0800

    test

---
+++ src/Notification.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: zhangzhenqian
+ * Date: 2017/12/8
+ * Time: 下午2:31
+ */
+
+namespace fire;
+
+
+class Notification
+{
+  public function send() {
+
+    echo 'send ok';
+  }
+}
